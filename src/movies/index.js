085d69797f601c47d535188b6e3eb61eec31020b
Ziang Huang <91920008+OliverZiangHuang@users.noreply.github.com>
2022-05-13 23:00:11 +0100

    Updated Movies Endpoints

--- src/movies/index.js
+++ src/movies/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
 
-
 const router = express.Router(); 
 router.get('/', (req, res) => {
     res.json(movies);
