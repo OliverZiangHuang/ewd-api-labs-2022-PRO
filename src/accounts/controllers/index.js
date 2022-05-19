c92f70e266d66f89463b6a5cc3598073199a8d9d
Ziang Huang <91920008+OliverZiangHuang@users.noreply.github.com>
2022-05-19 21:37:35 +0100

    Adding prepareation for account authent(logger)

--- src/accounts/controllers/index.js
+++ src/accounts/controllers/index.js
@@ -1,18 +1,19 @@
 import accountService from "../services";
-import bcrypt from 'bcryptjs';
-
+import logger from '../../utils/logger';
 export default (dependencies) => {
 
-    const createAccount = async (request, response, next) => {
-        // Input
-        const { firstName, lastName, email, password } = request.body;
-        // Treatment
-        const account = await accountService.registerAccount(firstName, lastName, email, password, dependencies);
-        //const output = dependencies.accountsSerializer.serialize(account);
-        //output
-        response.status(201).json(account)
+    const createAccount = async (request, response, next) => {  
+        
+        logger.customLogger.info('Call : Create Account Controler');
+        //console.log("---- Account Contoller---Create Account--");           
+        const { firstName, lastName, email, password } = request.body;        
+        const account = await accountService.registerAccount(firstName, lastName, email, password, dependencies);  
+          
+        response.status(201).json(account);
     };
     const getAccount = async (request, response, next) => {
+        logger.customLogger.info('Call : Get Account Controler');
+        //console.log("---- Account Contoller---Get Account--");           
         //input
         const accountId = request.params.id;
         // Treatment
@@ -22,82 +23,104 @@ export default (dependencies) => {
         response.status(200).json(account);
     };
     const listAccounts = async (request, response, next) => {
+        logger.customLogger.info('Call : List Account Controler');
+        //console.log("---- Account Contoller---List Account--");           
         // Treatment
-        const accounts = await accountService.find(dependencies);
+        const accounts = await accountService.find(dependencies); 
         //output
         response.status(200).json(accounts);
     };
-    const updateAccount = async (request, response, next) => {
-        // Input
-        const id = request.params.id;
-        const { firstName, lastName, email, password } = request.body;
-    //TODO - You implement the rest
-        const account = await accountService.registerAccount(id, firstName, lastName, email, password, dependencies);
-    //const output = dependencies.accountsSerializer.serialize(account);
-    //output
-        response.status(201).json(account)
+    const getByEmail = async (request, response, next) => {
+        logger.customLogger.info('Call : Account by Email Controler');
+        //console.log("---- Account Contoller---getBy Eamil--");           
+        const emailId = request.params.id;    
+        const account = await accountService.getByEmail(emailId, dependencies); 
+        response.status(200).json(account);
     };
-
     const authenticateAccount = async (request, response, next) => {
-        try {
+        try {            
+            //console.log("---- Auth Account ---");  
+            logger.customLogger.info('Call : Authenticate Account Controler');
             const { email, password } = request.body;
-            const token = await accountService.authenticate(email, password, dependencies);
+            const token = await accountService.authenticate(email, password, dependencies);                                   
             response.status(200).json({ token: `BEARER ${token}` });
         } catch (error) {
             response.status(401).json({ message: 'Unauthorised' });
         }
     };
+    const verifyToken = async (request, response, next) => {
+        try { 
+        // Input
+        const authHeader = request.headers.authorization;
 
+        // Treatment
+        //console.log("---- VerifyToken Account Controller 2 ---");
+        logger.customLogger.info('Call : Verify Token Account Controler');
+        //console.log(authHeader);
+        
+        const accessToken = authHeader.split(" ")[1];  // Example : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1haWxAZ21haWwuY29tIiwiaWF0IjoxNjU"
+        //console.log("-----Access Token -> " + accessToken);
+        logger.customLogger.verbose('Access Token : '+ accessToken);
+        const user = await accountService.verifyToken(accessToken, dependencies);
+
+        //output
+        next();
+    }catch(err){
+        //Token Verification Failed
+        logger.customLogger.error(`Token Verification Failed ${err.message}`);
+        next(new Error(`Verification Failed ${err.message}`));
+        }
+    };
     const addFavourite = async (request, response, next) => {
         try {
-            const { movieId } = request.body;
+            logger.customLogger.info('Call : Add Favourite Account Controler');
+            //console.log(dependencies);
+
+            const { movieId } = request.body;                
             const id = request.params.id;
             const account = await accountService.addFavourite(id, movieId, dependencies);
             response.status(200).json(account);
         } catch (err) {
+            logger.customLogger.error(`Favourite Add : Invalid Data ${err.message}`);
             next(new Error(`Invalid Data ${err.message}`));
         }
     };
     const getFavourites = async (request, response, next) => {
         try {
             const id = request.params.id;
+            logger.customLogger.info('Call : Get Favourite Account Controler');
+            //console.log("Control getFavourites"); 
             const favourites = await accountService.getFavourites(id, dependencies);
             response.status(200).json(favourites);
         } catch (err) {
+            logger.customLogger.error(`Favourite Add : Invalid Data ${err.message}`);
             next(new Error(`Invalid Data ${err.message}`));
         }
     };
 
-    const verifyToken = async (request, response, next) => {
-        try { 
-        // Input
-        const authHeader = request.headers.authorization; 
-         // Treatment
-
-        if (!authHeader || !authHeader.startsWith('BEARER ')){
-            next(new Error('Token Verification Failed/Invalid Token'))
-        // response.status(403).json({message:"Forbidden"});
-        }
-      
-        const accessToken = authHeader.split(" ")[1];
-        await accountService.verifyToken(accessToken, dependencies);
-
-        //output
-        next();
-    }catch(err){
-        //Token Verification Failed
-        next(new Error(`Verification Failed ${err.message}`));
+    const deleteFavourites = async (request, response, next) => {
+        try {
+            const id = request.params.id;
+            const movieid = request.params.movieid;
+            //console.log("Control delet Favourites"); 
+            logger.customLogger.info('Call : Delete Favourite Account Controler');
+            const favourites = await accountService.deleteFavourites(id, movieid, dependencies);
+            response.status(200).json(favourites);
+        } catch (err) {
+            logger.customLogger.error(`Favourite Delete : Invalid Data ${err.message}`);
+            next(new Error(`Invalid Data ${err.message}`));
         }
     };
 
     return {
+        createAccount,
+        getAccount,    
+        listAccounts,
+        getByEmail,
+        authenticateAccount,
+        verifyToken,
         addFavourite,
         getFavourites,
-        authenticateAccount,
-        createAccount,
-        getAccount,
-        listAccounts,
-        updateAccount,
-        verifyToken
+        deleteFavourites
     };
 };
--- src/accounts/repositories/mongo/AccountRepository.js
+++ src/accounts/repositories/mongo/AccountRepository.js
@@ -1,6 +1,7 @@
-import Account from '../../entities/Account';
+import Account from '../../entities/Accounts';
 import mongoose from 'mongoose';
 import AccountRepository from '../Repository';
+import logger from '../../../utils/logger';
 
 export default class extends AccountRepository {
 
@@ -15,35 +16,56 @@ export default class extends AccountRepository {
         });
         this.model = mongoose.model('Account', accountsSchema);
     }
-
+ 
     async persist(accountEntity) {
+        // const {firstName, lastName, email, password} = accountEntity;
+        // const result = new this.model({firstName, lastName, email, password});
+        // await result.save();
+        // accountEntity.id=result.id;
+        // return accountEntity;
+        logger.customLogger.info('Account Repo: Persist');
         const {firstName, lastName, email, password} = accountEntity;
-        const result = new this.model({firstName, lastName, email, password});
-        await result.save();
-        accountEntity.id=result.id;
-        return accountEntity;
+        const mongooseContact = new this.model({ firstName, lastName, email, password});
+        await mongooseContact.save();
+        accountEntity.id=mongooseContact.id;
+        return new Account(mongooseContact.id, mongooseContact.firstName, mongooseContact.lastName, mongooseContact.email, mongooseContact.password);
     }
 
     async merge(accountEntity) {
+        logger.customLogger.info('Account Repo: Merge');
         const {id, firstName, lastName, email, password, favourites } = accountEntity;
         await this.model.findByIdAndUpdate(id, { firstName, lastName, email, password, favourites });
-        console.log({id, firstName, lastName, email, password, favourites });
+        //console.log({id, firstName, lastName, email, password, favourites });
         return accountEntity;
     }
 
     async remove(userId) {
+        logger.customLogger.info('Account Repo: Remove');
         return this.model.findOneAndDelete(userId);
     }
 
+    async deleteFavourites(userId, moviesId) {
+         logger.customLogger.info('Account Repo: Delete Favourite');
+        const result =await this.model.findOneAndUpdate({_id:userId},
+        { $pull: { favourites: moviesId } });  
+        const {id, firstName, lastName, email, password, favourites } = result;   
+        return new Account(id, firstName, lastName, email, password, favourites );
+    }
+
     async get(userId) {
+        logger.customLogger.info('Account Repo: Get Users');
+        //console.log(userId); 
         const result = await this.model.findById(userId);
+   
         const {id, firstName, lastName, email, password, favourites } = result;
         return new Account(id, firstName, lastName, email, password, favourites );
     }
 
     async getByEmail(userEmail) {
+        logger.customLogger.info('Account Repo: Get Users by email id');
         const result = await this.model.findOne({email: userEmail});
-        return new Account(result.id, result.firstName, result.lastName, result.email, result.password,result.favourites);
+
+         return new Account(result.id, result.firstName, result.lastName, result.email, result.password,result.favourites);
     }
 
     async find() {
@@ -52,4 +74,13 @@ export default class extends AccountRepository {
             return new Account(result.id, result.firstName, result.lastName, result.email, result.password, result.favourites);
         });
     }
+
+    async getFavourites(userId) {
+        logger.customLogger.info('Account Repo: Get Favourite');
+        const result = await this.model.findById(userId);
+   
+        const {id, firstName, lastName, email, password, favourites } = result;
+        return new Account(id, firstName, lastName, email, password, favourites );
+    }    
+    
 }
